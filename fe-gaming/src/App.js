f1e46fad353bb3ea93b9f1556c1b2da3c8517785
shubhmpandey77 <cse.19bcs4577@gmail.com>
2025-03-27 19:52:39 +0530

    React setup done and bootstrap configured

---
+++ fe-gaming/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1>Hello Gaming Dashboard</h1>
+      <button className="btn btn-primary">Play Now</button>
+    </div>
+  );
+}
+
+export default App;
